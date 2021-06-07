f45fcc4c8e4768360f06353b75c1bdb715c63f96
Sriram Sami <frizensami@gmail.com>
2021-06-07 20:46:12 +0800

    Add deploy and move strings out

--- bot.py
+++ bot.py
@@ -10,22 +10,7 @@ from telegram.ext import (
 import telegram
 import sys
 from scraper import get_bloodstocks
-
-HELLO_MSG = """Welcome to the SG Blood Stocks Bot!
-
-We update you on the blood stock levels of the Singapore Red Cross.
-
-You can also subscribe to updates for specific blood types so you know when to donate (Coming soon!).
-
-You can use the /check command to check blood stock levels manually."""
-
-ABOUT_MSG = """SG Blood Stocks Bot Version 0.1 (Pre-Release)
-
-This bot retrieves blood stock data from https://www.redcross.sg/
-
-Please report any bugs to the email at https://sriramsami.com.
-
-Alternatively, open a bug report at https://github.com/frizensami/sgbloodstocksbot/issues"""
+from strings import HELLO_MSG, ABOUT_MSG
 
 STOCKS_STR = None
 
---
+++ strings.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+HELLO_MSG = """Welcome to the SG Blood Stocks Bot!
+
+We update you on the blood stock levels of the Singapore Red Cross.
+
+You can also subscribe to updates for specific blood types so you know when to donate (Coming soon!).
+
+You can use the /check command to check blood stock levels manually."""
+
+ABOUT_MSG = """SG Blood Stocks Bot Version 0.2 (Pre-Release)
+
+This bot retrieves blood stock data from https://www.redcross.sg/
+
+Please report any bugs to the email at https://sriramsami.com.
+
+Alternatively, open a bug report at https://github.com/frizensami/sgbloodstocksbot/issues"""
